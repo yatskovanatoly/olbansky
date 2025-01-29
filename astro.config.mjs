983c52a31b4c247c37af355609c077d0586d5e9e
Anatoly <yatskovanatoly@gmail.com>
2025-01-29 07:40:57 +0300

    тест

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://yatskovanatoly.github.io'
-});
+export default defineConfig({})
