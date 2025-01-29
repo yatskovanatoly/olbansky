1a33fba08b9d02f3c717a54c393e955b592fe7a0
Anatoly <yatskovanatoly@gmail.com>
2025-01-29 07:33:19 +0300

    билд

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yatskovanatoly.github.io',
-  base: '/olbansky'
+  site: 'https://yatskovanatoly.github.io'
 });
