c09c2ef27977448a14bad7cfd85008325fa5be15
Anatoly <yatskovanatoly@gmail.com>
2026-02-11 18:31:48 +0300

    minor styling; prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yatskovanatoly.github.io',
-  base: '/olbansky'
+  site: "https://yatskovanatoly.github.io",
+  base: "/olbansky",
 });
