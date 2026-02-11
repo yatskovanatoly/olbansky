52ed24fea41a716ff4980adf5f68f48f621d710c
Anatoly <yatskovanatoly@gmail.com>
2026-02-12 01:53:46 +0300

    clean code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yatskovanatoly.github.io",
-  base: "/olbansky",
-  // env: {
-  //   schema: {
-  //     UPLOADTHING_TOKEN: envField.string({ context: "server", access: "secret" }),
-  //     UPLOADTHING_APP_ID: envField.string({ context: "server", access: "secret" }),
-  //   }
-  // }
-});
+	site: 'https://yatskovanatoly.github.io',
+	base: '/olbansky',
+})
